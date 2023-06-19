61628f53458f685bd38382b87f78b0b23c29a233
tawhidib <tawhidib@gmail.com>
2023-06-19 22:35:10 +0600

    pref: confing service add

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.enableCors();
+
+  // setting global prefix... ... ...
+  app.setGlobalPrefix(configService.get('GLOBAL_PREFIX'));
+  await app.listen(configService.get('PORT') || 4000);
 }
 bootstrap();
