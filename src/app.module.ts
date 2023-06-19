5929f199a44b12e54f5a2fd025044fe83af7c0e7
tawhidib <tawhidib@gmail.com>
2023-06-19 22:49:30 +0600

    feat: resources created for media via nest cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database.module';
+import { MediaModule } from './media/media.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, MediaModule],
   controllers: [AppController],
   providers: [AppService],
 })
