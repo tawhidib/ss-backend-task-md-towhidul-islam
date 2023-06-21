9d42c5a6529217d311354d5dae4df27573d12531
tawhidib <tawhidib@gmail.com>
2023-06-21 23:33:48 +0600

    feat: create dto with validators for media

--- src/media/dto/create-media.dto.ts
+++ src/media/dto/create-media.dto.ts
@@ -1,1 +1,33 @@
-export class CreateMediaDto {}
+import {
+  ArrayNotEmpty,
+  IsArray,
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+
+export class CreateMediaDto {
+  @IsNotEmpty({ message: 'Provide Media name' })
+  name: string;
+
+  @IsNotEmpty({ message: 'Provide Media release date' })
+  @IsDateString({}, { message: 'Provide date in ISO string formate' })
+  releaseDate: Date;
+
+  @IsNumber({}, { message: 'Provide runtime of this media' })
+  runtime: number;
+
+  @IsArray({ message: 'Provide actors in correct formate' })
+  @ArrayNotEmpty({ message: 'Actors should not empty' })
+  @IsString({ each: true, message: 'Actor name should be string' })
+  actors: string[];
+
+  @IsNotEmpty({ message: 'Provide director name' })
+  @IsString({ message: 'Director name should be string' })
+  director: string;
+
+  @IsNotEmpty({ message: 'Provide producer name' })
+  @IsString({ message: 'Producer name should be string' })
+  producer: string;
+}
