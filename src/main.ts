3ddd504b0fb281a4fb500221b2ba8a58accaed2f
tawhidib <tawhidib@gmail.com>
2023-06-21 23:13:37 +0600

    feat(main): add validation pipe for global with necessary options

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.enableCors();
+
+  // add global validation with options
+  app.useGlobalPipes(
+    new ValidationPipe({
+      enableDebugMessages: true,
+      skipUndefinedProperties: false,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      dismissDefaultMessages: true,
+    }),
+  );
 
   // setting global prefix... ... ...
   app.setGlobalPrefix(configService.get('GLOBAL_PREFIX'));
