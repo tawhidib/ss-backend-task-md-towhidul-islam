54c7edb39b95254306f6bfd7baadd08dd75bcb79
tawhidib <tawhidib@gmail.com>
2023-06-21 23:04:33 +0600

    refactor(media.schema): add timestamps

--- src/media/schemas/media.schema.ts
+++ src/media/schemas/media.schema.ts
@@ -3,7 +3,7 @@ import { Document } from 'mongoose';
 
 export type MediaDocument = Media & Document;
 
-@Schema()
+@Schema({ timestamps: true })
 export class Media {
   @Prop({ required: true })
   name: string;
